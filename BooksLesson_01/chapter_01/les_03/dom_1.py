38e9dadefe6fbb4422ea51617e3483bc1bcc8270
Pavlov Nikolay <pavlov80nikolay@gmail.com>
2021-02-14 10:46:36 +0300

    Python. Book 1, house_work 1.

---
+++ BooksLesson_01/chapter_01/les_03/dom_1.py
@@ -0,0 +1,6 @@
+print('Albert Einstein once said, "A person who never made\n'
+      'a mistake never tried anything new."')
+
+famous_person = 'Albert Einstein'
+print(f'{famous_person} once said, "A person who never made\n'
+      'a mistake never tried anything new."')
