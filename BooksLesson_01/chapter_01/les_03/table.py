545dc61ca1507f60c9f2626ab088484cb6ce87ee
Pavlov Nikolay <pavlov80nikolay@gmail.com>
2021-02-14 08:29:40 +0300

    Python. Book 1, lesson 3-2.

---
+++ BooksLesson_01/chapter_01/les_03/table.py
@@ -0,0 +1,10 @@
+# Табуляция
+print('Python')
+print('\tPython')
+
+# Разрывы строк - точнее переносы с новой строки
+print('Languages:\nPython\nC\nJavaScript')
+
+# Сочетание
+print('Languages:\n\tPython\n\tC\n\tJavaScript')
+
