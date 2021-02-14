f05e5a8884e736cdac4a8ee3b8e20ea2ce0e06b8
Pavlov Nikolay <pavlov80nikolay@gmail.com>
2021-02-14 08:57:17 +0300

    Python. Book 1, lesson 3-3.

---
+++ BooksLesson_01/chapter_01/les_03/strip.py
@@ -0,0 +1,15 @@
+# удаление пробелов
+
+favorite_language = ' python '
+print(favorite_language)
+
+# с правого края
+print(favorite_language.rstrip())
+
+# с левого края
+print(favorite_language.lstrip())
+
+# с обоих
+print(favorite_language.strip())
+
+# далее лучше воспользоваться ПЕРЕМЕННЫМи, что бы записать результат удаление пробелов
