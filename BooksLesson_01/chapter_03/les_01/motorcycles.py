5892c08b7df902a801c0b306d1c3ed208cc817d1
Pavlov80Nikolay <pavlov80nikolay@gmail.com>
2021-02-15 16:07:14 +0300

    Python. Book 1, lesson 3-1-2

--- BooksLesson_01/chapter_03/les_01/motorcycles.py
+++ BooksLesson_01/chapter_03/les_01/motorcycles.py
@@ -1,3 +1,34 @@
 # Изменение, добавление и удаление элементов
 # Изменение элементов списка
 
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+# Добавление элеметов в список
+
+motorcycles.append('honda')
+print(motorcycles)
+
+# метод append() упращает динамическое построение списков. Вы можете начать с пустого списка и добавить в него элеметы
+motor = []
+motor.append('honda')
+motor.append('ducati')
+motor.append('yamaha')
+print(motor)
+
+# Вставка элемента в список
+motor.insert(0, 'suzuki')
+print(motor)
+
+# Удаление элементов из списка
+del motor[0]
+print(motor)
+
+# Уделение элементов по значению
+motor.remove('ducati')
+print(motor)
+
+# Извлечение элементов из произвольной позиции списка
