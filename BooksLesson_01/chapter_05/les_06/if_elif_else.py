962136e52a4bd8b2463d681af7796c56a9e8fb2b
Pavlov80Nikolay <pavlov80nikolay@gmail.com>
2021-02-20 15:07:44 +0300

    Python. Book 1, lesson 5-6-2

--- BooksLesson_01/chapter_05/les_06/if_elif_else.py
+++ BooksLesson_01/chapter_05/les_06/if_elif_else.py
@@ -1,10 +1,22 @@
 # цепочки if-elif-else
 
+# Первый способ цепочки команд IF
+# age = 12
+#
+# if age < 4:
+#     print('Your admission cost is $0.')
+# elif age < 18:
+#     print('Your admission cost is $25.')
+# else:
+#     print('Your admission cost is $40.')
+
+# А можно написать и так, так короче написания программы
 age = 12
 
 if age < 4:
-    print('Your admission cost is $0.')
+    price = 0
 elif age < 18:
-    print('Your admission cost is $25.')
+    price = 25
 else:
-    print('Your admission cost is $40.')
+    price = 40
+print(f'Your admission cost is ${price}.')
