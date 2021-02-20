81cdc84d35faf5f5b81b7a828b5369ee9edaf549
Pavlov80Nikolay <pavlov80nikolay@gmail.com>
2021-02-20 15:04:12 +0300

    Python. Book 1, lesson 5-6

---
+++ BooksLesson_01/chapter_05/les_06/if_elif_else.py
@@ -0,0 +1,10 @@
+# цепочки if-elif-else
+
+age = 12
+
+if age < 4:
+    print('Your admission cost is $0.')
+elif age < 18:
+    print('Your admission cost is $25.')
+else:
+    print('Your admission cost is $40.')
