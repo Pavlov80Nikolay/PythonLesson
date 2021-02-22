dc26a36ed36ebbab0eeb41eab7cbea5851cac6df
Pavlov80Nikolay <pavlov80nikolay@gmail.com>
2021-02-22 13:36:47 +0300

    Python. Book 1, lesson 6-1

---
+++ BooksLesson_01/chapter_06/les_01/alien.py
@@ -0,0 +1,16 @@
+# создание простого словаря
+
+alien_0 = {
+    'color': 'green',
+    'points': 5,
+}
+
+print(alien_0['color'])
+print(alien_0['points'])
+
+# обращение к значениям в словаре
+# извлекает значение, связаное с ключем 'points'
+
+new_points = alien_0['points']
+print(f'You just earned {new_points} points!')
+
