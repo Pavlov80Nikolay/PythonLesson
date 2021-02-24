5a23702de7ed012be317155cac3d96e4a85f6590
Pavlov80Nikolay <pavlov80nikolay@gmail.com>
2021-02-24 17:05:53 +0300

    Python. Book 1, lesson 8-2

---
+++ BooksLesson_01/chapter_08/les_02/pets.py
@@ -0,0 +1,8 @@
+# позиционные аргументы
+
+def describe_pet(animal_type, pet_name):
+    """Выводит информацию о животном."""
+    print(f'\nI have a {animal_type}.')
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet('hamster', 'harry')
