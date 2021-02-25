6ae3f95fd234aa2dd6b7a03d839777c0cbf7bc15
Pavlov80Nikolay <pavlov80nikolay@gmail.com>
2021-02-25 11:50:10 +0300

    Python. Book 1, lesson 9-2-1

--- BooksLesson_01/chapter_09/les_02/car.py
+++ BooksLesson_01/chapter_09/les_02/car.py
@@ -8,11 +8,18 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """Возвращает аккуратно отформатированное описание."""
         long_name = f'{self.year} {self.make} {self.model}'
         return long_name.title()
 
+    def read_odometer(self):
+        """Выводит пробег машины в милях."""
+        print(f'This car has {self.odometer_reading} miles on it.')
+
 my_new_car = Car('audi', 'a4', 2019)
 print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+
