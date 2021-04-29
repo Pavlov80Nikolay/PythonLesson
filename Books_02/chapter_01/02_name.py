1f883271bad88124a5f29180e1c661942de5d373
Nikolay Pavlov <pavlov80nikolay@gmail.com>
2021-04-29 11:54:03 +0300

    02-02 - книга 2 по Python

---
+++ Books_02/chapter_01/02_name.py
@@ -0,0 +1,34 @@
+# TODO Строки, изменение строк
+
+# Табуляции и разрывы строк, разрыв строк добавляется - \n
+
+x = 'Languages:\nPython\nC\nJavaScript'
+print(x)
+
+# а табуляция - \t
+
+y = 'Languages:\n\tPython\n\tC\n\tJavaScript'
+print(y)
+
+
+# удаление пропусков (более пременитеьно для паролей и логинов)
+# метод rstrip() - удаляет пробелы у правого края (в конце)
+
+favorite_language = 'Python  '
+print(favorite_language.rstrip())
+
+# lstrip() - удаляет у левого края (в начале)
+# strip() - удаляет пропуски с обоих концев
+
+
+
+
+
+
+
+
+
+
+
+
+
