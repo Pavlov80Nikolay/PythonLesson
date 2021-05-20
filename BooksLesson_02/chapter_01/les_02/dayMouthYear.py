89360bb64206734f12b7129eeded8b94fcb9a2d1
Nikolay Pavlov <pavlov80nikolay@gmail.com>
2021-05-20 09:54:15 +0300

    Книга 02-02-2 Вывод числа, месяца и года по отдельности

---
+++ BooksLesson_02/chapter_01/les_02/dayMouthYear.py
@@ -0,0 +1,17 @@
+import datetime
+
+# вывод отдельно даты дня
+x = datetime.date.today().day
+
+# вывод отдельно даты месяца
+y = datetime.date.today().month
+
+# вывод отдельно даты года
+z = datetime.date.today().year
+
+print(x)
+
+print(y)
+
+print(z)
+
