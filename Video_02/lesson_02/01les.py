dd86f70354afab57623c9ff7dfd8324d95b45c9e
Nikolay Pavlov <pavlov80nikolay@gmail.com>
2021-05-20 14:44:34 +0300

    2 - 1, видео 2

---
+++ Video_02/lesson_02/01les.py
@@ -0,0 +1,23 @@
+
+
+print('Hello world!!!')
+print('Hello world!!!')
+
+# вывод слова в 2-ых кавычках
+print('"Hello!"')
+
+# вывод со всеми кавычками
+print('"Hell\'s hey"')
+
+# Разные способы вывода строк в Python3
+print('Hello ' + 'Alex')
+
+print('Hello','Alex')
+
+x = 'Hello'
+y = 'Alex'
+print(f'{x} {y}')
+
+# sep=' ' - указывает как разделять строки (тут ставит пробелы)
+print('Hello,','Alex','and Michel', sep=' ')
+
