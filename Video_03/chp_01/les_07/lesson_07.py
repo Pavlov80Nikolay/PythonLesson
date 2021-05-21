cbbd9c06f6730eae491c3b0217ddbc94e96f0899
Nikolay Pavlov <pavlov80nikolay@gmail.com>
2021-05-21 14:06:04 +0300

    1 - 7, видео 3

---
+++ Video_03/chp_01/les_07/lesson_07.py
@@ -0,0 +1,26 @@
+# Булев тип данных
+
+my_true = True
+my_false = False
+
+print(type(my_true))
+print(type(my_false))
+
+# str() int() float() bool() - преводит к типу данных
+x = 5.2
+print(type(x))
+print(x)
+
+x = int(x)
+print(type(x))
+print(x)
+
+x = float(x)
+print(type(x))
+print(x)
+
+x = str(x)
+print(type(x))
+print(x)
+
+
